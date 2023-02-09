7fd46bbd5a52900ac22f0144c66123746c6961cc
JulieSparrow <vitalievna@list.ru>
2023-02-10 01:17:16 +0700

    Добавление задачи Range, часть 1

---
+++ Range/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Range range = new Range(13.25, 18.15);
+
+        System.out.println("Задан диапазон " + range.getFrom() + " " + range.getTo());
+        System.out.println("Длина диапазона " + range.getLength());
+
+        double number = 14.05;
+
+        if (range.isInside(number)) {
+            System.out.println("Число принадлежит указанному диапазону");
+        } else {
+            System.out.println("Число не принадлежит указанному диапазону");
+        }
+    }
+}
